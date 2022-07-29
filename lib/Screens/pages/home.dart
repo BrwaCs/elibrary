5f598423182be987ef3f3010f88708d992d99620
BrwaCs <brwanahman19@gmail.com>
2022-07-29 17:54:56 +0300

    complete home page

--- lib/Screens/pages/home.dart
+++ lib/Screens/pages/home.dart
@@ -124,7 +124,7 @@ class _homeState extends State<home> {
         ),
       ),
       
-      body: Column(
+      body: ListView(
         children: [
           Center(
             child: Padding(
@@ -157,6 +157,77 @@ class _homeState extends State<home> {
               padding: const EdgeInsets.only(left:10.0),
               child: Text(
                 "Suggestion",
+                style: TextStyle(
+                  fontSize:16,
+                  fontWeight: FontWeight.w500
+                ),
+              ),
+            )
+          ],
+        ),
+     
+     Container(
+       height: 200,
+       width: double.infinity,
+       child: ListView.builder(
+         
+         scrollDirection: Axis.horizontal,
+         itemCount: book_mockdata.length,
+         itemBuilder: ((context, index) {
+           List<book_datamodel> _bookmodel=book_mockdata.map((element){
+             return book_datamodel.fromMap(element);
+           }).toList();
+           return 
+          Padding(
+           padding: const EdgeInsets.all(12.0),
+           child: Row(
+             children: [
+               Container(
+                 height: 180,
+                 width: 100,
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        children: [
+                          Container(
+                            height: 133,
+                            width: 81,
+                            child:FittedBox(
+                              child: Image.network(_bookmodel[index].image.toString()),
+                              fit: BoxFit.fill,
+                            ) 
+                          )
+                        ],
+                      ),
+                    ),
+                    SizedBox(height:10),
+                    Row(
+                      children: [
+                        Text(_bookmodel[index].book_name.toString()),
+                      ],
+                    )
+                  ],
+                ),
+               ),
+              
+               
+             ],
+           ),
+         );
+       
+         }
+         )
+       ),
+     ),
+     SizedBox(height: 10,),
+          Row(
+          mainAxisAlignment:MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left:10.0),
+              child: Text(
+                "Newest",
                 style: TextStyle(
                   fontSize:16,
                   fontWeight: FontWeight.w500
