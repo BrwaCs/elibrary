1bcb75ea2c4eedda859705cc91cfa62edb76a390
BrwaCs <brwanahman19@gmail.com>
2022-09-03 18:53:23 +0300

    another commit in review page and profile

--- lib/Screens/pages/SecondProfile.dart
+++ lib/Screens/pages/SecondProfile.dart
@@ -6,8 +6,10 @@ import 'dart:io';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elibrary/Auth/login.dart';
 import 'package:elibrary/Screens/Provider/user_provider.dart';
+import 'package:elibrary/Screens/pages/profile.dart';
 
 import 'package:elibrary/Screens/widgets/Loding_indicater.dart';
+import 'package:elibrary/dataModels/ReviewDataModel.dart';
 import 'package:elibrary/dataModels/User_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -192,6 +194,219 @@ builder: (context,snapshot) {
                 )
               ],
             ),
+
+
+
+
+
+             SizedBox(height: 20,),
+
+FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            future: FirebaseFirestore.instance.collection("Review").get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingIndicator();
+              } else if (snapshot.hasError) {
+                return Center(child: Text("Error..."));
+              } else if (snapshot.data == null) {
+                return Center(child: Text("Data is null"));
+              }
+// create a liat of book models from fire store  query snapshot
+              List<ReviewDataModel> Review_model = snapshot.data!.docs
+                  .map((e) => ReviewDataModel.fromMap(e.data()))
+                  .toList();
+              
+  return   Expanded(
+    child: ListView.builder(
+     
+                       itemCount: Review_model.length,
+                      itemBuilder: (context, index) {
+      return FirebaseAuth.instance.currentUser!.uid != Review_model[index].uid ?  Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Container(
+                      height: 175,
+                      width: 340,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                         color: Color.fromARGB(255, 235, 235,235),
+                      ), 
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+                                 height: 38,
+                                 width: 320,
+                                 child: Row(
+                                   children: [
+                                         Container(
+                               
+                                child: Review_model[index].userImage?.toString()=="null" ?
+                                GestureDetector(
+                                   onTap: () {
+                                    if(FirebaseAuth.instance.currentUser!.uid== Review_model[index].uid){
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                   }else{
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondProfile()));
+                                   }
+                                                 },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left:8.0,top:7,right: 5),
+                                    child: Container(
+                                      height: 30,
+                                      width: 30,
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      image: DecorationImage(
+                                        
+                                        fit: BoxFit.fill,
+                                        image:
+                                            AssetImage("assets/images/profile.png"),
+                                      ),
+                                    ),
+                                    ),
+                                  ),
+                                )
+                                : GestureDetector(
+                                   onTap: () {
+                                     if(FirebaseAuth.instance.currentUser!.uid== Review_model[index].uid){
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                   }else{
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondProfile()));
+                                   }
+                                                 },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left:8.0,top:7,right: 5),
+                                    child: Container(
+                                       height: 30,
+                                    width: 30,
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      image: DecorationImage(
+                                        
+                                        fit: BoxFit.fill,
+                                        image:
+                                            NetworkImage(Review_model[index].userImage.toString()),
+                                      ),
+                                    ),
+                                    ),
+                                  ),
+                                )
+             
+                              ),
+                                    //  Padding(
+                                    //    padding: const EdgeInsets.all(8.0),
+                                    //    child: GestureDetector(
+                                    //    onTap: () {
+                                    // Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                    //              },
+                                    // child: Icon(
+                                    // IconData(0xee35, fontFamily: 'MaterialIcons'),
+                                    // size: 26.0,   
+                                    //    ),
+                                    //    ),
+                                    //  ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(top:10),
+                                       child: Text("${Review_model[index].userName}",
+                                       style: TextStyle(
+                                         fontSize: 14,fontWeight: FontWeight.normal
+                                       ),),
+                                     ),
+     
+     
+                                   ],
+                                 ),
+                               )
+                               )
+                            ],
+                          ),
+                          Divider(
+                            thickness: 0.8,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+     
+                                 height: 66,
+                                 width: 320,
+                                 child: Row( 
+                                  mainAxisAlignment:MainAxisAlignment.spaceAround,
+                                  children: <Widget>[ 
+                                  // Container( width: 14.0, ),
+                                 Flexible( 
+                                child: Text("${Review_model[index].review}",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                              fontSize: 14,
+                             height: 1.25
+                       )
+                       ),
+                     ) 
+                     ], 
+                     ), 
+                               )
+                               )
+                            ],
+                          ),
+                           Divider(
+                            thickness: 0.8,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+                               
+                                 height: 35,
+                                 width: 320,
+                                 child: Center(
+                                   child: Text("${Review_model[index].bookName}", 
+                                   style: 
+                                   TextStyle(
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.normal,
+                                   ),),
+                                 ),
+                               )
+                               )
+                            ],
+                          ),
+                        ],
+                      )
+                      ),
+                    ),
+                  ),
+                
+          
+              ]
+              
+            )
+            :Container();
+  
+  
+                      }
+    ),
+  );
+ 
+            }
+ )
             
           ],
         
--- lib/Screens/pages/profile.dart
+++ lib/Screens/pages/profile.dart
@@ -5,8 +5,11 @@ import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:elibrary/Auth/login.dart';
+import 'package:elibrary/Screens/pages/SecondProfile.dart';
+import 'package:elibrary/Screens/pages/review.dart';
 import 'package:elibrary/Screens/pages/update_profile.dart';
 import 'package:elibrary/Screens/widgets/Loding_indicater.dart';
+import 'package:elibrary/dataModels/ReviewDataModel.dart';
 import 'package:elibrary/dataModels/User_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
@@ -197,13 +200,6 @@ builder: (context,snapshot) {
                  ],             
                )
                 
-              //    Container( child:CircleAvatar(
-              //    backgroundColor: Colors.transparent,
-              //    radius: 60,
-              //     backgroundImage:
-              //     NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjC0aAIzmF7cePLylt4ObVinrZRkqGn-4Gv3fHf7J4fQHyppZp_MZ8HQm2KtQCPvfWIyQ&usqp=CAU'),
-                
-              //  ))
                :Stack(
                  children: [
                      Container(
@@ -249,19 +245,6 @@ builder: (context,snapshot) {
               )
             ],
             ),
-            // SizedBox(height: 10,),
-            // Row(
-            //   mainAxisAlignment:MainAxisAlignment.center,
-            //   children: [
-            //     Text(
-            //       "Reviews 10",
-            //       style: TextStyle(
-            //         fontSize:16,
-            //         fontWeight: FontWeight.bold
-            //       ),
-            //     )
-            //   ],
-            // ),
             SizedBox(height: 10,),
             Padding(
               padding: const EdgeInsets.only(left:10.0),
@@ -313,7 +296,238 @@ builder: (context,snapshot) {
                 )
               ],
             ),
-            
+            SizedBox(height: 20,),
+
+FutureBuilder<QuerySnapshot<Map<String, dynamic>>>(
+            future: FirebaseFirestore.instance.collection("Review").get(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return LoadingIndicator();
+              } else if (snapshot.hasError) {
+                return Center(child: Text("Error..."));
+              } else if (snapshot.data == null) {
+                return Center(child: Text("Data is null"));
+              }
+// create a liat of book models from fire store  query snapshot
+              List<ReviewDataModel> Review_model = snapshot.data!.docs
+                  .map((e) => ReviewDataModel.fromMap(e.data()))
+                  .toList();
+              
+  return   Expanded(
+    child: ListView.builder(
+     
+                       itemCount: Review_model.length,
+                      itemBuilder: (context, index) {
+      return FirebaseAuth.instance.currentUser!.uid == Review_model[index].uid ?  Column(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(13.0),
+                    child: Container(
+                      height: 175,
+                      width: 340,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                         color: Color.fromARGB(255, 235, 235,235),
+                      ), 
+                      child: Column(
+                        children: [
+                          Row(
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+                                 height: 38,
+                                 width: 320,
+                                 child: Row(
+                                   children: [
+                                         Container(
+                               
+                                child: Review_model[index].userImage?.toString()=="null" ?
+                                GestureDetector(
+                                   onTap: () {
+                                     if(FirebaseAuth.instance.currentUser!.uid== Review_model[index].uid){
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                   }else{
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondProfile()));
+                                   }
+                                                 },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left:8.0,top:7,right: 5),
+                                    child: Container(
+                                      height: 30,
+                                      width: 30,
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      image: DecorationImage(
+                                        
+                                        fit: BoxFit.fill,
+                                        image:
+                                            AssetImage("assets/images/profile.png"),
+                                      ),
+                                    ),
+                                    ),
+                                  ),
+                                )
+                                : GestureDetector(
+                                   onTap: () {
+                                      if(FirebaseAuth.instance.currentUser!.uid== Review_model[index].uid){
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                   }else{
+
+                                  Navigator.push(context, MaterialPageRoute(builder: (context)=>SecondProfile()));
+                                   }
+                                                 },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(left:8.0,top:7,right: 5),
+                                    child: Container(
+                                       height: 30,
+                                    width: 30,
+                                      decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(50),
+                                      image: DecorationImage(
+                                        
+                                        fit: BoxFit.fill,
+                                        image:
+                                            NetworkImage(Review_model[index].userImage.toString()),
+                                      ),
+                                    ),
+                                    ),
+                                  ),
+                                )
+             
+                              ),
+                                    //  Padding(
+                                    //    padding: const EdgeInsets.all(8.0),
+                                    //    child: GestureDetector(
+                                    //    onTap: () {
+                                    // Navigator.push(context, MaterialPageRoute(builder: (context)=>Profile()));
+                                    //              },
+                                    // child: Icon(
+                                    // IconData(0xee35, fontFamily: 'MaterialIcons'),
+                                    // size: 26.0,   
+                                    //    ),
+                                    //    ),
+                                    //  ),
+                                     Padding(
+                                       padding: const EdgeInsets.only(top:10),
+                                       child: Text("${Review_model[index].userName}",
+                                       style: TextStyle(
+                                         fontSize: 14,fontWeight: FontWeight.normal
+                                       ),),
+                                     ),
+     
+     
+                                   ],
+                                 ),
+                               )
+                               )
+                            ],
+                          ),
+                          Divider(
+                            thickness: 0.8,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+     
+                                 height: 66,
+                                 width: 320,
+                                 child: Row( 
+                                  mainAxisAlignment:MainAxisAlignment.spaceAround,
+                                  children: <Widget>[ 
+                                  // Container( width: 14.0, ),
+                                 Flexible( 
+                                child: Text("${Review_model[index].review}",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                              fontSize: 14,
+                             height: 1.25
+                       )
+                       ),
+                     ) 
+                     ], 
+                     ), 
+                               )
+                               )
+                            ],
+                          ),
+                           Divider(
+                            thickness: 0.8,
+                            color: Colors.black,
+                          ),
+                          Row(
+                            
+                            children: [
+                             Flexible( 
+                            fit: FlexFit.tight,
+                               child: Container(
+                               
+                                 height: 35,
+                                 width: 320,
+                                 child: Center(
+                                   child: Text("${Review_model[index].bookName}", 
+                                   style: 
+                                   TextStyle(
+                                     fontSize: 16,
+                                     fontWeight: FontWeight.normal,
+                                   ),),
+                                 ),
+                               )
+                               )
+                            ],
+                          ),
+                        ],
+                      )
+                      ),
+                    ),
+                  ),
+                
+          
+              ]
+              
+            )
+            :Container();
+  
+  
+                      }
+    ),
+  );
+ 
+            }
+ )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
           ],
         
         );
@@ -379,16 +593,5 @@ uploadImage() async {
       print("Permission not granted. Try Again with permission access");
     }
   }
-  //   updateProfile(BuildContext context) async {
-  //   Map<String, dynamic> map = Map();
-  //   if (fileName != null) {
-  //     String url = await uploadImage();
-  //     map['imageUrl'] = url;
-  //   }
-  //   await FirebaseFirestore.instance
-  //       .collection("user")
-  //       .doc(FirebaseAuth.instance.currentUser!.uid)
-  //       .update(map);
-  
-  // }
+
 }
